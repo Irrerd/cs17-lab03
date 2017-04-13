712d304cca51719e783a0fd757b592fa3b89360c
u211-04 <u211-04@public.mpei.local>
2017-04-13 16:01:36 +0300

    Preobrazovanie temperaturi

--- main.cpp
+++ main.cpp
@@ -52,6 +52,34 @@ void test_temperature_input(){
     assert(t.scale == 'Kelvin');
 }
 
+temperature convert (const temperature& from, Scale scale) {
+    double kelvin;
+    switch (from.scale){
+    case Kelvin:
+        kelvin = from.value;
+        break;
+    case Farengheit:
+        kelvin = (from.value - 32)/1,8 + 273,15;
+        break;
+    case Celsius:
+        kelvin = from.value + 273,15;
+        break;
+    }
+    temperature result;
+    result.scale = scale;
+    switch (scale){
+    case Kelvin:
+        result.value = kelvin;
+        break;
+    case Farengheit:
+        result.value = kelvin + 273,15;
+        break;
+    case Celsius:
+        result.value = 1,8*kelvin + 32 ;
+        break;
+    }
+    return result;
+}
 
 int
 main() {
