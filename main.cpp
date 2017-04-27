1c54b18e969d4e379e3326c4419368210f85e0f5
u211-04 <u211-04@public.mpei.local>
2017-04-27 14:58:29 +0300

    Realizacia funkciy

--- main.cpp
+++ main.cpp
@@ -4,24 +4,6 @@
 #include <cassert>
 #include "temperature.h"
 using namespace std;
-istream & operator >>
-        (istream & in, temperature& t){
-    in>>t.value;
-    char symbol;
-    in>>symbol;
-    switch (symbol){
-    case 'K':
-        t.scale = Kelvin;
-        break;
-    case 'C':
-        t.scale = Celsius;
-        break;
-    case 'F':
-        t.scale = Farengheit;
-        break;
-    }
-    return in;
-}
 
 void test_temperature_input(){
     stringstream in ("30C");
@@ -40,46 +22,6 @@ void test_temperature_input(){
     assert(t.value == 0);
     assert(t.scale == Kelvin);
 }
-
-temperature convert (const temperature& from, Scale scale) {
-    double kelvin;
-    switch (from.scale){
-    case Kelvin:
-        kelvin = from.value;
-        break;
-    case Farengheit:
-        kelvin = (from.value - 32)/1,8 + 273,15;
-        break;
-    case Celsius:
-        kelvin = from.value + 273,15;
-        break;
-    }
-    temperature result;
-    result.scale = scale;
-    switch (scale){
-    case Kelvin:
-        result.value = kelvin;
-        break;
-    case Farengheit:
-        result.value = kelvin + 273,15;
-        break;
-    case Celsius:
-        result.value = 1,8*kelvin + 32 ;
-        break;
-    }
-    return result;
-}
-
-bool  operator<(temperature& lhs, temperature& rhs){
-    lhs = convert(lhs, rhs.scale);
-    return lhs.value < rhs.value;
-}
-
-bool  operator>(temperature& lhs, temperature& rhs){
-    lhs = convert(lhs, rhs.scale);
-    return lhs.value > rhs.value;
-}
-
 
 int
 main() {
